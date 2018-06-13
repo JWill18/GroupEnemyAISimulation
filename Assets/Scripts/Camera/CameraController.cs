498a47a1d4403c8f8c413687c47c5a2e1414c744
Jesse Willis <jessewillis827@gmail.com>
2018-06-12 23:03:12 -0400

    Add apropriate namespaces for Camera and Player scripts

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -2,117 +2,91 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[ExecuteInEditMode]
-public class CameraController : MonoBehaviour {
-
-	/// <summary>
-	/// The angle that the camera will be at.
-	/// </summary>
-	public Quaternion CameraRotation;
-
-	private Camera mainCamera = null;
-
-	private void Awake()
+namespace GroupEnemyAISimulation.Assets.Scripts.Camera
+{
+	[ExecuteInEditMode]
+	public class CameraController : MonoBehaviour
 	{
-		// If this object has the camera script already on it.
-		if (gameObject.GetComponent<Camera>() != null)
+
+		/// <summary>
+		/// The angle that the camera will be at.
+		/// </summary>
+		public Quaternion CameraRotation;
+
+		private UnityEngine.Camera mainCamera = null;
+
+		private void Awake()
 		{
-			// Find the main camera
-			if (gameObject.GetComponent<Camera>().tag == "MainCamera")
+			// If this object has the camera script already on it.
+			if (gameObject.GetComponent<UnityEngine.Camera>() != null)
 			{
-				mainCamera = gameObject.GetComponent<Camera>();
-			}
-			else
-			{
-				var cameraList = FindObjectsOfType<Camera>();
-
-				foreach (var camera in cameraList)
+				// Find the main camera
+				if (gameObject.GetComponent<UnityEngine.Camera>().tag == "MainCamera")
 				{
-					if (camera.tag == "MainCamera")
-					{
-						mainCamera = camera;
-					}
+					mainCamera = gameObject.GetComponent<UnityEngine.Camera>();
 				}
-			}
-
-			// If main camera is not found then this is the main camera. Else remove self
-			if (mainCamera == null)
-			{
-				mainCamera = gameObject.GetComponent<Camera>();
-				mainCamera.tag = "MainCamera";
-			}
-			else
-			{
-				if (mainCamera.gameObject.GetComponent<CameraController>() == null)
+				else
 				{
-					var tempCamera = mainCamera.gameObject.AddComponent<CameraController>();
-					tempCamera.CameraRotation = CameraRotation;
-					print("There is already a main camera in the level. There can only be one main camera at a time.");
-					Destroy(this);
-				}
-			}
+					var cameraList = FindObjectsOfType<UnityEngine.Camera>();
 
-			GameObject cameraContainer;
-			CameraController cameraMovement;
-
-			// Find or create empty parent object.
-			if (transform.parent == null)
-			{
-				cameraContainer = new GameObject("CameraContainer");
-				transform.parent = cameraContainer.transform;
-			}
-			else
-			{
-				cameraContainer = transform.parent.gameObject;
-			}
-
-			// Find or Add camera movement script to the parent object.
-			if (cameraContainer.GetComponent<CameraController>() == null)
-			{
-				cameraMovement = cameraContainer.AddComponent<CameraController>();
-			}
-			else
-			{
-				cameraMovement = cameraContainer.GetComponent<CameraController>();
-			}
-
-			cameraMovement.CameraRotation = CameraRotation;
-			DestroyImmediate(this);
-		}
-		else
-		{
-			if (gameObject.GetComponentInChildren<Camera>() == null)
-			{
-				var cameraList = FindObjectsOfType<Camera>();
-
-				foreach (var camera in cameraList)
-				{
-					if (camera.tag == "MainCamera")
+					foreach (var camera in cameraList)
 					{
-						mainCamera = camera;
+						if (camera.tag == "MainCamera")
+						{
+							mainCamera = camera;
+						}
 					}
 				}
 
+				// If main camera is not found then this is the main camera. Else remove self
 				if (mainCamera == null)
 				{
-					mainCamera = new Camera
+					mainCamera = gameObject.GetComponent<UnityEngine.Camera>();
+					mainCamera.tag = "MainCamera";
+				}
+				else
+				{
+					if (mainCamera.gameObject.GetComponent<CameraController>() == null)
 					{
-						tag = "MainCamera"
-					};
+						var tempCamera = mainCamera.gameObject.AddComponent<CameraController>();
+						tempCamera.CameraRotation = CameraRotation;
+						print("There is already a main camera in the level. There can only be one main camera at a time.");
+						Destroy(this);
+					}
 				}
 
-				mainCamera.transform.parent = gameObject.transform;
-				mainCamera.transform.position = gameObject.transform.position;
+				GameObject cameraContainer;
+				CameraController cameraMovement;
+
+				// Find or create empty parent object.
+				if (transform.parent == null)
+				{
+					cameraContainer = new GameObject("CameraContainer");
+					transform.parent = cameraContainer.transform;
+				}
+				else
+				{
+					cameraContainer = transform.parent.gameObject;
+				}
+
+				// Find or Add camera movement script to the parent object.
+				if (cameraContainer.GetComponent<CameraController>() == null)
+				{
+					cameraMovement = cameraContainer.AddComponent<CameraController>();
+				}
+				else
+				{
+					cameraMovement = cameraContainer.GetComponent<CameraController>();
+				}
+
+				cameraMovement.CameraRotation = CameraRotation;
+				DestroyImmediate(this);
 			}
 			else
 			{
-				var childCamera = gameObject.GetComponentInChildren<Camera>();
-
-				if (childCamera.tag != "MainCamera")
+				if (gameObject.GetComponentInChildren<UnityEngine.Camera>() == null)
 				{
-					DestroyImmediate(childCamera);
-
-					var cameraList = FindObjectsOfType<Camera>();
+					var cameraList = FindObjectsOfType<UnityEngine.Camera>();
 
 					foreach (var camera in cameraList)
 					{
@@ -124,31 +98,63 @@ public class CameraController : MonoBehaviour {
 
 					if (mainCamera == null)
 					{
-						mainCamera = new Camera
+						mainCamera = new UnityEngine.Camera
 						{
 							tag = "MainCamera"
 						};
 					}
 
-
 					mainCamera.transform.parent = gameObject.transform;
 					mainCamera.transform.position = gameObject.transform.position;
 				}
+				else
+				{
+					var childCamera = gameObject.GetComponentInChildren<UnityEngine.Camera>();
+
+					if (childCamera.tag != "MainCamera")
+					{
+						DestroyImmediate(childCamera);
+
+						var cameraList = FindObjectsOfType<UnityEngine.Camera>();
+
+						foreach (var camera in cameraList)
+						{
+							if (camera.tag == "MainCamera")
+							{
+								mainCamera = camera;
+							}
+						}
+
+						if (mainCamera == null)
+						{
+							mainCamera = new UnityEngine.Camera
+							{
+								tag = "MainCamera"
+							};
+						}
+
+
+						mainCamera.transform.parent = gameObject.transform;
+						mainCamera.transform.position = gameObject.transform.position;
+					}
+				}
 			}
 		}
-	}
 
-	// Use this for initialization
-	void Start () {
-
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		if(mainCamera != null && mainCamera.transform.rotation != CameraRotation)
+		// Use this for initialization
+		void Start()
 		{
-			mainCamera.transform.rotation = CameraRotation;
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+			if (mainCamera != null && mainCamera.transform.rotation != CameraRotation)
+			{
+				mainCamera.transform.rotation = CameraRotation;
+			}
 		}
 	}
 }
--- Assets/Scripts/Player/PlayerControls.cs
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -2,28 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControls : MonoBehaviour {
+namespace GroupEnemyAISimulation.Assets.Scripts.Player
+{
+	public class PlayerControls : MonoBehaviour
+	{
 
-	/// <summary>
-	/// The speed that the camera moves.
-	/// </summary>
-	public float MovementSpeed;
+		/// <summary>
+		/// The speed that the camera moves.
+		/// </summary>
+		public float MovementSpeed;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetAxis("Vertical") != 0)
+		// Use this for initialization
+		void Start()
 		{
-			transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
+
 		}
 
-		if (Input.GetAxis("Horizontal") != 0)
+		// Update is called once per frame
+		void Update()
 		{
-			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime);
+			if (Input.GetAxis("Vertical") != 0)
+			{
+				transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
+			}
+
+			if (Input.GetAxis("Horizontal") != 0)
+			{
+				transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime);
+			}
 		}
 	}
 }
