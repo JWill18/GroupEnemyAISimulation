193b194a8a920e28d4559c517d424dae400c95e4
Jesse Willis <jessewillis827@gmail.com>
2018-06-12 22:42:12 -0400

    Created Player Controller to handle movement controls

---
+++ Assets/Scripts/Player/PlayerControls.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControls : MonoBehaviour {
+
+	/// <summary>
+	/// The speed that the camera moves.
+	/// </summary>
+	public float MovementSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis("Vertical") != 0)
+		{
+			transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * MovementSpeed * Time.deltaTime);
+		}
+
+		if (Input.GetAxis("Horizontal") != 0)
+		{
+			transform.Translate(Vector3.right * Input.GetAxis("Horizontal") * MovementSpeed * Time.deltaTime);
+		}
+	}
+}
