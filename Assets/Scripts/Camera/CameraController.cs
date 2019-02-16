1f38a1a219a322114970d71d3d6a0525c338d238
Jesse Willis <jessewillis827@gmail.com>
2019-02-16 17:19:59 -0500

    Comment on the Camera controls

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -5,12 +5,24 @@ namespace GroupEnemyAISimulation.Assets.Scripts.Camera
 {
 	public class CameraController : MonoBehaviour
 	{
+		/// <summary>
+		/// The object that the camera will be focused on
+		/// </summary>
 		public PlayerControls FocusObject;
 
+		/// <summary>
+		/// The amount of room that the object has in the x axis to move before the camera will follow
+		/// </summary>
 		public float CameraDistanceLagX;
 
+		/// <summary>
+		/// The amount of room that the object has in the z axis to move before the camera will follow
+		/// </summary>
 		public float CameraDistanceLagZ;
 
+		/// <summary>
+		/// The movement speed of the camera
+		/// </summary>
 		public float MoveSpeed;
 
 		// Use this for initialization
@@ -22,64 +34,100 @@ namespace GroupEnemyAISimulation.Assets.Scripts.Camera
 		// Update is called once per frame
 		void Update()
 		{
-			if(!IsPlayerInView())
+			// If the object is not within view. Adjust camera position
+			if(!IsObjectInView())
 			{
+				// Find the difference in camera position and the focus object position
 				var distanceFromFocusObject = FocusObject.transform.position - transform.position;
 
+				// determines how to move and in what axis
 				var moveX = 0.0f;
 				var moveZ = 0.0f;
 
-				if(!IsPlayerInViewNegativeX(distanceFromFocusObject.x))
+				// If the object is out of view in the x axis, determine which direction the camera will need to go
+				if(!IsObjectInViewNegativeX(distanceFromFocusObject.x))
 				{
 					moveX = -MoveSpeed;
 				}
-				else if (!IsPlayerInViewPositiveX(distanceFromFocusObject.x))
+				else if (!IsObjectInViewPositiveX(distanceFromFocusObject.x))
 				{
 					moveX = MoveSpeed;
 				}
 
-				if(!IsPlayerInViewNegativeZ(distanceFromFocusObject.z))
+				// If the object is out of view in the z axis, determine which direction the camera will need to go
+				if (!IsObjectInViewNegativeZ(distanceFromFocusObject.z))
 				{
 					moveZ = -MoveSpeed;
 				}
-				else if (!IsPlayerInViewPositiveZ(distanceFromFocusObject.z))
+				else if (!IsObjectInViewPositiveZ(distanceFromFocusObject.z))
 				{
 					moveZ = MoveSpeed;
 				}
 
+				// Move the camera towards the object
 				transform.Translate(moveX * Time.deltaTime, 0, moveZ * Time.deltaTime);
 			}
 		}
 
-		private bool IsPlayerInView()
+		#region Camera Detection
+
+		/// <summary>
+		/// Checks to see if the Focus Object is within sight of the camera 
+		/// </summary>
+		/// <returns>The camera is within acceptable bounds of the camera's view</returns>
+		private bool IsObjectInView()
 		{
+			// Finds the distance between the focus object and the camera object
 			var distanceFromFocusObject = transform.position - FocusObject.transform.position;
 
-			var withinXView = IsPlayerInViewNegativeX(distanceFromFocusObject.x) && IsPlayerInViewPositiveX(distanceFromFocusObject.x);
+			// Check to see if the object is within view in the x axis
+			var withinXView = IsObjectInViewNegativeX(distanceFromFocusObject.x) && IsObjectInViewPositiveX(distanceFromFocusObject.x);
 
-			var withinZView = IsPlayerInViewNegativeZ(distanceFromFocusObject.z) && IsPlayerInViewPositiveZ(distanceFromFocusObject.z);
+			// Check to see if the object is within view in the z axis
+			var withinZView = IsObjectInViewNegativeZ(distanceFromFocusObject.z) && IsObjectInViewPositiveZ(distanceFromFocusObject.z);
 
 			return withinXView && withinZView;
 		}
 
-		private bool IsPlayerInViewPositiveX(float xDifference)
+		/// <summary>
+		/// Checks to see if the Focus Object is within sight of the camera in the positive x axis
+		/// </summary>
+		/// <param name="xDifference">The difference value between the camera location and the focus object location in the x axis</param>
+		/// <returns>The camera is with acceptable bounds of the camera's view in the positive x axis</returns>
+		private bool IsObjectInViewPositiveX(float xDifference)
 		{
 			return xDifference < CameraDistanceLagX;
 		}
 
-		private bool IsPlayerInViewNegativeX(float xDifference)
+		/// <summary>
+		/// Checks to see if the Focus Object is within sight of the camera in the negative x axis
+		/// </summary>
+		/// <param name="xDifference">The difference value between the camera location and the focus object location in the x axis</param>
+		/// <returns>The camera is with acceptable bounds of the camera's view in the negative x axis</returns>
+		private bool IsObjectInViewNegativeX(float xDifference)
 		{
 			return xDifference > -CameraDistanceLagX;
 		}
 
-		private bool IsPlayerInViewPositiveZ(float ZDifference)
+		/// <summary>
+		/// Checks to see if the Focus Object is within sight of the camera in the positive z axis
+		/// </summary>
+		/// <param name="ZDifference">The difference value between the camera location and the focus object location in the z axis</param>
+		/// <returns>The camera is with acceptable bounds of the camera's view in the positive z axis</returns>
+		private bool IsObjectInViewPositiveZ(float ZDifference)
 		{
 			return ZDifference < CameraDistanceLagZ;
 		}
 
-		private bool IsPlayerInViewNegativeZ(float ZDifference)
+		/// <summary>
+		/// Checks to see if the Focus Object is within sight of the camera in the negative x axis
+		/// </summary>
+		/// <param name="ZDifference">The difference value between the camera location and the focus object location in the z axis</param>
+		/// <returns>The camera is with acceptable bounds of the camera's view in the negative z axis</returns>
+		private bool IsObjectInViewNegativeZ(float ZDifference)
 		{
 			return ZDifference > 5.0f;
 		}
+		#endregion
 	}
 }
